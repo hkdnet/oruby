aace715014ffad3f568c9ff4c1dacfa46ca65551
hkdnet <satoko.itse@gmail.com>
2017-09-30 01:59:53 +0900

    Update

--- main.rb
+++ main.rb
@@ -1,5 +1,26 @@
+require 'pry'
+require 'ripper'
+
+def reduce(sexp)
+  puts '-' * 20
+  p sexp
+  if sexp.first == :program
+    return sexp[1].map { |e| reduce e }.first
+  end
+  case sexp.first
+  when :@int
+    sexp[1].to_i
+  when :binary
+    l, o, r = sexp.drop(1)
+    reduce(l) + reduce(r) # currently, only support :+
+  else
+    binding.pry
+  end
+end
+
 def evaluate(text)
-  text.to_i
+  sexp = Ripper.sexp(text)
+  reduce(sexp)
 end
 
 filename = ARGV.first
